5a35d4951db5fa3d9aeaee280b8499c501409ddf
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2026-02-10 13:44:57 -0500

    set version metadata in artifacts

--- cmd/mcp-tools/main.go
+++ cmd/mcp-tools/main.go
@@ -4,12 +4,14 @@ import (
 	"os"
 
 	"github.com/michaelquigley/df/dl"
+	"github.com/openziti/mcp-gateway/build"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "mcp-tools",
-	Short: "Access MCP tooling through MCP Gateway",
+	Use:     "mcp-tools",
+	Short:   "Access MCP tooling through MCP Gateway",
+	Version: build.String(),
 }
 
 func main() {
